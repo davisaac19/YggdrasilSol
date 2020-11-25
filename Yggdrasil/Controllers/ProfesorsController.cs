dde9795b224378be5361c6f5efb43130c67ebcae
David May <davisaac8@gmail.com>
2020-11-25 12:57:32 -0600

    Se agrega profesor.

---
+++ Yggdrasil/Controllers/ProfesorsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Yggdrasil.Context;
+using Yggdrasil.Models.DBModels;
+
+namespace Yggdrasil.Controllers
+{
+    public class ProfesorsController : Controller
+    {
+        private readonly SchoolDatabaseContext _context;
+
+        public ProfesorsController(SchoolDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Profesors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Profesors.ToListAsync());
+        }
+
+        // GET: Profesors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profesor = await _context.Profesors
+                .FirstOrDefaultAsync(m => m.IdProfesor == id);
+            if (profesor == null)
+            {
+                return NotFound();
+            }
+
+            return View(profesor);
+        }
+
+        // GET: Profesors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profesors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProfesor,NameProfesor,EnrollDate")] Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(profesor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profesor = await _context.Profesors.FindAsync(id);
+            if (profesor == null)
+            {
+                return NotFound();
+            }
+            return View(profesor);
+        }
+
+        // POST: Profesors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdProfesor,NameProfesor,EnrollDate")] Profesor profesor)
+        {
+            if (id != profesor.IdProfesor)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(profesor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfesorExists(profesor.IdProfesor))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profesor = await _context.Profesors
+                .FirstOrDefaultAsync(m => m.IdProfesor == id);
+            if (profesor == null)
+            {
+                return NotFound();
+            }
+
+            return View(profesor);
+        }
+
+        // POST: Profesors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var profesor = await _context.Profesors.FindAsync(id);
+            _context.Profesors.Remove(profesor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfesorExists(int id)
+        {
+            return _context.Profesors.Any(e => e.IdProfesor == id);
+        }
+    }
+}
